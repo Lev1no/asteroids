229f18201095d3852d18d503383025e276aef586
Lev1no <leevi.alanen1@gmail.com>
2025-06-04 15:58:12 +0300

    sprites

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #game init
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #time for fps
+    clock = pygame.time.Clock()
+    dt = 0
 
     running = True
     while running:
@@ -15,8 +20,12 @@ def main():
             if event.type == pygame.QUIT:
                return
 
+        #game logic and drawing 
         screen.fill((0, 0, 0))  
         pygame.display.flip()
 
+        #tick the clock to maintain 60fps and get delta time
+        dt = clock.tick(60) / 1000 #convert ms to s
+
 if __name__ == "__main__":
-    main()   
+    main()     
